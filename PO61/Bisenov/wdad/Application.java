f404bac32b0cc893689d20aa42d33e62d2d7cd02
VBisenov <37118694+VBisenov@users.noreply.github.com>
2018-12-10 12:16:59 +0400

    Delete Application.java

--- PO61/Bisenov/wdad/Application.java
+++
@@ -1,8 +0,0 @@
-package PO61.Bisenov.wdad;
-
-public class Application {
-
-    public static void main(String[] args) {
-        System.out.println("Im Bisenov Vladimir, and Im not a monkey");
-    }
-}
