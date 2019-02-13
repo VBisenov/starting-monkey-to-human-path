af413c1f0755e19d67b5034d5aa659398785391c
VBisenov <v.bisenov@mail.ru>
2019-02-13 19:59:25 +0400

    added load object from xml file

--- PO61/Bisenov/wdad/learn/xml/Organization.java
+++ PO61/Bisenov/wdad/learn/xml/Organization.java
@@ -3,9 +3,10 @@ package PO61.Bisenov.wdad.learn.xml;
 import javax.xml.bind.annotation.*;
 import java.util.ArrayList;
 
-@XmlRootElement(namespace = "C:\\Users\\Владимир\\Desktop\\Учёба\\3 курс\\Java\\starting-monkey-to-human-path\\src\\PO61\\Bisenov\\wdad\\learn\\xml\\organization.xml")
+@XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(propOrder = {"name", "departments"})
+//@XmlType(propOrder = {"name", "departments"})
 public class Organization {
     private String name;
 
@@ -85,5 +86,14 @@ public class Organization {
                 department.remove(firstName,secondName);
             }
         }
+    }
+
+    public boolean contains(Department department){
+        for (Department d: departments){
+            if (department.equals(d)){
+                return true;
+            }
+        }
+        return false;
     }
 }
