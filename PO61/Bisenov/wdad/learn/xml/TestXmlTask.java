2886c3fb0de587b3cf1c5c939a890ddf7f16f8e2
VBisenov <v.bisenov@mail.ru>
2019-02-24 18:17:52 +0400

    jdbcTask

--- PO61/Bisenov/wdad/learn/xml/TestXmlTask.java
+++ PO61/Bisenov/wdad/learn/xml/TestXmlTask.java
@@ -2,10 +2,6 @@ package PO61.Bisenov.wdad.learn.xml;
 
 public class TestXmlTask {
     public static void main(String[] args) {
-<<<<<<<<< Temporary merge branch 1
-
-=========
->>>>>>>>> Temporary merge branch 2
         XmlTask xmlTask = new XmlTask();
         System.out.println(xmlTask.salaryAverage());
         System.out.println(xmlTask.salaryAverage("Software"));
