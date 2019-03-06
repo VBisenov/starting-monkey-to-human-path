3f0bd53ed4e6413148a9cd9e75b78dbb3086e1c5
VBisenov <v.bisenov@mail.ru>
2019-03-06 21:08:32 +0400

    jdbc task 1.0

--- PO61/Bisenov/wdad/dbscripts/InsertIntoDBScript.java
+++ PO61/Bisenov/wdad/dbscripts/InsertIntoDBScript.java
@@ -17,9 +17,17 @@ public class InsertIntoDBScript {
             DriverManager.registerDriver(new Driver());
             Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
             Statement statement = connection.createStatement();
-            String[] rows = {"id", "name"};
-            String[] values = {"1", "manager"};
-            insertData("jobtitles", rows, values, statement);
+            
+            String[] jbRows = {"id", "name"};
+            String[] jbValues = {"1", "manager"};
+            insertData("jobtitles",jbRows, jbValues, statement);
+            String[] empRows = {"id", "first_name", "second_name", "birth_date",
+                    "hire_date", "salary", "jobtitles_id", "departments_id"};
+            String[] empValues = {"1", "Vova", "Bisenov", "18-06-1999", "06-03-2019", "20000", "1", "1"};
+            insertData("employees", empRows, empValues, statement);
+            String[] depRows = {"id", "name", "description"};
+            String[] depValues = {"1", "engineering", "department of engineering"};
+            insertData("departments", depRows, depValues, statement);
         } catch (SQLException ex){
             ex.printStackTrace();
         }
@@ -37,9 +45,7 @@ public class InsertIntoDBScript {
         for (String string: values){
             resultValuesSet.append(string).append(", ");
         }
-        resultValuesString = resultRowsSet.substring(0, resultRowsSet.length()-2); // removes the last comma from the values
-        //String query = "INSERT INTO "+tableName+"("+resultRowsString+") "+"VALUES"+" ("+resultValuesString+")";
-
+        resultValuesString = resultRowsSet.substring(0, resultRowsSet.length()-2); // removes the last comma from the value
        statement.executeUpdate("INSERT INTO "+tableName+"("+resultRowsString+")"+"VALUES"+"("+resultValuesString+")");
     }
 }
