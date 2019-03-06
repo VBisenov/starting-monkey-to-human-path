2941e06a71a2435e926444dce2230c05f653620b
VBisenov <v.bisenov@mail.ru>
2019-03-06 20:36:11 +0400

    jdbc task 1.0

--- PO61/Bisenov/wdad/dbscripts/CreateDBScript.java
+++ PO61/Bisenov/wdad/dbscripts/CreateDBScript.java
@@ -5,9 +5,10 @@ import com.mysql.jdbc.Driver;
 import java.sql.*;
 
 public class CreateDBScript {
-    static final String DB_URL = "jdbc:mysql://localhost:3306/";
-    static final String USER = "root";
-    static final String PASS = "qwerty";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/";
+    private static final String USER = "root";
+    private static final String PASS = "qwerty";
+    private static final String dbName = "organization";
 
     public static void main(String[] args) {
         Connection connection = null;
@@ -18,13 +19,20 @@ public class CreateDBScript {
             statement = connection.createStatement();
 
             System.out.print("Creating DataBase...");
-            statement.executeUpdate("drop database organization");
-            createDataBase("organization", statement);
+            statement.executeUpdate("drop database "+dbName); //todo delete this line
+            createDataBase(dbName, statement);
             System.out.println(" OK");
 
-        //    statement.executeQuery("SELECT * FROM employee"); TODO проверить работу и ResultSet
-            String[] rows = {"firstname varchar(50)", "secondname varchar(50)"};
-            createTable("employee", rows, statement);
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+dbName, USER, PASS); // connecting to
+            statement = connection.createStatement();                                                        // already created DB
+
+            String[] empRows = {"id integer(10)", "first_name varchar(50)", "second_name varchar(50)", "birth_date date",
+                    "hire_date date", "salary numeric(15, 5)", "jobtitles_id integer(10)", "departments_id integer(10)"};
+            createTable("employees", empRows, statement);
+            String[] depRows = {"id integer(10)", "name varchar(50)", "description varchar(255)"};
+            createTable("departments", depRows, statement);
+            String[] jtRows = {"id integer(10)", "name varchar(100)"};
+            createTable("jobtitles", jtRows, statement);
 
         } catch (SQLException ex){
             ex.printStackTrace();
@@ -47,17 +55,17 @@ public class CreateDBScript {
         }
     }
     private static void createTable(String name, String[] rows, Statement statement) throws SQLException{
+        String resultRowsString = null;
         if (rows == null){
             System.out.println("You haven't insert rows in the table");
             return;
         }
         StringBuilder resultRowsSet = new StringBuilder();
         for (String str: rows){
-            if (str != null){
-                resultRowsSet.append(", ").append(str);
-            }
+                resultRowsSet.append(str).append(", ");
         }
-        String query = ("CREATE TABLE "+name+" ("+resultRowsSet+")");
+        resultRowsString = resultRowsSet.substring(0, resultRowsSet.length()-2); // removes the last comma from the rows
+        String query = ("CREATE TABLE "+name+" ("+resultRowsString+")");
         statement.executeUpdate(query);
     }
 
--- PO61/Bisenov/wdad/dbscripts/InsertIntoDBScript.java
+++ PO61/Bisenov/wdad/dbscripts/InsertIntoDBScript.java
@@ -1,12 +1,8 @@
 package PO61.Bisenov.wdad.dbscripts;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-
 public class InsertIntoDBScript {
-    Connection connection = null;
-    PreparedStatement statement = null;
     public static void main(String[] args) {
+
 
     }
 }
